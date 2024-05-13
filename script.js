45665e24b62068d6c654e122f4ed9f2ba63aaff7
Ana Carla Da Hora <93679007+AnaCarladaHora@users.noreply.github.com>
2024-05-13 16:39:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const scriptURL = 'https:https://script.google.com/macros/s/AKfycbyc7s-efHb9XbkAkCpnU0BnxYS99zf0yNPRxnhW2d84r029Nk0gqkT8La0gzhYFxp3PEQ/exec'; // Substitua YOUR_SCRIPT_ID_HERE pelo ID do seu script do Google Apps Script
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyc7s-efHb9XbkAkCpnU0BnxYS99zf0yNPRxnhW2d84r029Nk0gqkT8La0gzhYFxp3PEQ/exec'; // Substitua YOUR_SCRIPT_ID_HERE pelo ID do seu script do Google Apps Script
 
-const form = document.forms[''cachorroPerdidoForm''];
+const form = document.forms["cachorroPerdidoForm"];
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
       // Exibir mensagem de sucesso ou erro ao usuário
-      if (response.ok) {
+      if (response.ok) { // Corrigido: Comparando com true implicitamente
         alert("Informações enviadas com sucesso!");
         form.reset();
       } else {
